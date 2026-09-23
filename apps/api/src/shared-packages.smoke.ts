61ad8ab2071024e0cee6242dae0eb5509f3a8507
peanutbear-du <peanutbear4025@gmail.com>
2026-09-23 12:02:54 +0800

    chore: add shared packages

---
+++ apps/api/src/shared-packages.smoke.ts
@@ -0,0 +1,5 @@
+import type {} from "@reward-planner/api-client";
+import type {} from "@reward-planner/business-constants";
+import type {} from "@reward-planner/schemas";
+import type {} from "@reward-planner/types";
+import type {} from "@reward-planner/utils";
